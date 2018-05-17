42fedc37a7b0f6fcf5dd6a72555c503825d40a69
Louis-Amas <Gradoula13?>
2018-05-17 11:39:24 +0200

    Added resume

--- cleanStep.sh
+++ cleanStep.sh
@@ -6,7 +6,7 @@ out=$2;
 
 echo 'To lower...';
 python3 src/toLower.py $corpus 1.tmp;
-echo 'Deleting hashtags, usernames, url links, RT and duplicate...';
+echo 'Deleting hashtags, usernames, url links, RT and duplicate from text...';
 python3 src/cleanData.py 1.tmp 2.tmp;
 echo 'Deleting tweets with uncommon hashtags...';
 python3 src/deleteUnCommonHashtags.py 2.tmp 500 $out;
